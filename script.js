6b14d7ec1cc1434a05b1a59b63e39dbe08064f9d
Alexandre Rogé--Lévêque <alexandreroglev59@gmail.com>
2026-03-15 10:49:16 +0100

    Fix selector for burger icon in script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log('connecté !')
 
 // Je sélectionne et je stocke
 // l'icône burger
-const icone = document.querySelector('navabar-mobile i');
+const icone = document.querySelector('.navbar-mobile i');
 console.log(icone);
 // la DIV modal
 const modal = document.querySelector('.modal');
